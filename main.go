2a8cbcc088b63641105f950d81c56a34164a6133
Kwame Akuamoah-Boateng <kaboateng14@gmail.com>
2024-10-29 20:26:20 +0000

    add graceful exiting and increase buffer size

--- main.go
+++ main.go
@@ -6,9 +6,12 @@ import (
 	"fmt"
 	"net"
 	"os"
+	"os/signal"
+	"syscall"
 )
 
 func main() {
+
 	if len(os.Args) == 1 {
 		fmt.Println("provide host address")
 		return
@@ -20,33 +23,44 @@ func main() {
 		return
 	}
 	defer conn.Close()
-	fmt.Printf("connected to %s...\n", os.Args[1])
-	reader := bufio.NewReader(os.Stdin)
-	var request []byte
+	fmt.Printf("connected to %s...\tPress Ctrl+C to quit\n", os.Args[1])
 
-	for {
-		line, err := reader.ReadBytes('\n')
-		if err != nil {
-			fmt.Println("error reading input:", err)
-			return
+	go func() {
+		for {
+			reader := bufio.NewReader(os.Stdin)
+			var request []byte
+
+			for {
+				line, err := reader.ReadBytes('\n')
+				if err != nil {
+					fmt.Println("error reading input:", err)
+					return
+				}
+
+				request = append(request, line...)
+
+				if bytes.Compare(line, []byte("\n")) == 0 {
+					break
+				}
+			}
+
+			_, err = conn.Write(request)
+			if err != nil {
+				fmt.Println("error sending request:", err)
+				return
+			}
+			buf := make([]byte, 4096)
+			_, err = conn.Read(buf)
+			if err != nil {
+				fmt.Println("error reading request:", err)
+			}
+			fmt.Printf("%s\n\n", string(buf))
 		}
+	}()
 
-		request = append(request, line...)
+	sigChan := make(chan os.Signal, 1)
+	signal.Notify(sigChan, syscall.SIGINT, syscall.SIGTERM)
+	_ = <-sigChan
+  fmt.Println("exiting...")
 
-		if bytes.Compare(line, []byte("\n")) == 0 {
-			break
-		}
-	}
-
-	_, err = conn.Write(request)
-	if err != nil {
-		fmt.Println("error sending request:", err)
-		return
-	}
-	buf := make([]byte, 1024)
-	_, err = conn.Read(buf)
-	if err != nil {
-		fmt.Println("error reading request:", err)
-	}
-	fmt.Println(string(buf))
 }
